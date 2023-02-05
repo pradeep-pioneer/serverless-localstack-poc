0759cfe11ab5a1715002aac86d482fae2f1bbce6
Pradeep Singh <contact@pradeep.vip>
2023-02-05 20:51:46 +0530

    finished the list jobs function

---
+++ 02-api-dynamodb-sqs-lambda-s3/jobs/list.ts
@@ -0,0 +1,36 @@
+'use strict';
+
+import { DynamoDB } from 'aws-sdk'
+
+let dynamoDb = new DynamoDB.DocumentClient();
+if(process.env.LOCALSTACK_HOSTNAME && process.env.LOCALSTACK_HOSTNAME!==''){
+  const options = {endpoint: `http://${process.env.LOCALSTACK_HOSTNAME}:4566`}
+  dynamoDb = new DynamoDB.DocumentClient(options);
+}
+const params = {
+  TableName: process.env.DYNAMODB_TABLE,
+};
+
+module.exports.list = (event, context, callback) => {
+  // fetch all jobs from the database
+  // For production workloads you should design your tables and indexes so that your applications can use Query instead of Scan.
+  dynamoDb.scan(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch the jobs items.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Items),
+    };
+    callback(null, response);
+  });
+};
